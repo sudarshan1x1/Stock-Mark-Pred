6c2791e724cdcf5d570f5969a767c972d20005b9
sudarshan1x1 <iamsudarshan.as@gmail.com>
2024-01-30 01:16:37 +0530

    eighth commit

--- main.py
+++ main.py
@@ -153,5 +153,5 @@ def get_image():
     return send_file('linear_reg_algo.png', mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
 
